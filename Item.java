df986d8be61f6297147a981384642f0f51c2606c
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2021-03-22 17:11:47 -0400

    Start of Item class

---
+++ Item.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private String itemDescription;
+    private int itemWeight;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+       
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String itemDescription(String itemDescription)
+    {
+        // put your code here
+        
+    }
+}
